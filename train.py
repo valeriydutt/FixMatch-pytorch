12a86aacee54eef529dd56ba8b8ba4d95fc4b9bc
valeriydutt <39168826+valeriydutt@users.noreply.github.com>
2021-02-18 08:35:44 +0000

    updated train.py with new dataset

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ def main():
     parser.add_argument('--num-workers', type=int, default=4,
                         help='number of workers')
     parser.add_argument('--dataset', default='cifar10', type=str,
-                        choices=['cifar10', 'cifar100'],
+                        choices=['cifar10', 'cifar100', 'drd'],
                         help='dataset name')
     parser.add_argument('--num-labeled', type=int, default=4000,
                         help='number of labeled data')
@@ -198,6 +198,16 @@ def main():
             args.model_cardinality = 8
             args.model_depth = 29
             args.model_width = 64
+
+    elif args.dataset == 'drd':
+    args.num_classes = 5
+    if args.arch == 'wideresnet':
+        args.model_depth = 28
+        args.model_width = 2
+    elif args.arch == 'resnext':
+        args.model_cardinality = 4
+        args.model_depth = 28
+        args.model_width = 4
 
     labeled_dataset, unlabeled_dataset, test_dataset = DATASET_GETTERS[args.dataset](
         args, './data')
